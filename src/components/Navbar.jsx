9bb8f806b98813e56ffce929ea655f8c62341a56
Kim Olson <kolson622@gmail.com>
2023-07-05 16:19:21 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks} from "../constants/index.js";
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+      <nav
+        className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      >
+        <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
+          <Link
+            to="/"
+            className="flex items-center gap-2"
+            onClick={() => {
+            setActive("");
+            window.scrollTo(0,0);
+            }}
+          >
+            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <p className="text-white text-[18px] font-bold cursor-pointer">kimberly <span className="sm:block hidden">| JavaScript Mastery</span></p>
+          </Link>
+          <p className="text-red-500">asdsa</p>
+        </div>
+      </nav>
   )
 }
 
