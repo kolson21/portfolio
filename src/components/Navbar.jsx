04ee16800da2a36eee224676926d041361cd269d
Kim Olson <kolson622@gmail.com>
2023-07-05 18:08:20 -0600

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,21 @@ const Navbar = () => {
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[18px] font-bold cursor-pointer">kimberly <span className="sm:block hidden">| JavaScript Mastery</span></p>
           </Link>
-          <p className="text-red-500">asdsa</p>
+          <ul className="list-none hidden sm:flex flex-row gap-10">
+            {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer`}
+                  onClick={() => setActive(link.title)}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+            ))}
+          </ul>
         </div>
       </nav>
   )
